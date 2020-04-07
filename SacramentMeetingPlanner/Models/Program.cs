2ea5f9410d31c72dade0fe9c6e83cd3a04b44c47
Swanson-Cody <35240552+Swanson-Cody@users.noreply.github.com>
2020-04-06 18:04:34 -0600

    Add project files.

---
+++ SacramentMeetingPlanner/Models/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentMeetingPlanner.Models
+{
+    public class Program
+    {
+        public int ProgramID { get; set; }
+
+        [Display(Name = "Date")]
+        [DataType(DataType.Date)]
+        public DateTime DateOfMeeting { get; set; }
+
+        [Display(Name = "Conductor")] public string ConductingLeader { get; set; }
+
+        [Display(Name = "Opening Song")]
+        public string OpeningSong { get; set; }
+
+        [Display(Name = "Opening Prayer")]
+        public string OpeningPrayer { get; set; }
+
+        [Display(Name = "Sacrament Hymn")]
+        public string SacramentHymn { get; set; }
+
+        public List<Speaker> Speakers { get; set; }
+
+        [Display(Name = "Intermediate Music")]
+        public string IntermediateMusic { get; set; }
+        
+        [Display(Name = "Closing Song")]
+        public string ClosingSong { get; set; }
+
+        [Display(Name = "Closing Prayer")]
+        public string ClosingPrayer { get; set; }
+    }
+}
---
+++ SacramentMeetingPlanner/Models/Speaker.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SacramentMeetingPlanner.Models
+{
+    public class Speaker
+    {
+        //primary key
+        public int SpeakerID { get; set; }
+        
+        //foreign key
+        public int ProgramId { get; set; }
+
+        [Display(Name = "First name of speaker")]
+        public string SpeakerFirstName { get; set; }
+
+        [Display(Name = "Last name of speaker")]
+        public string SpeakerLastName { get; set; }
+
+        public string TalkSubject { get; set; }
+    }
+}
